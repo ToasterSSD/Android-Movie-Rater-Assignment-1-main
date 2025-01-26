c3b31534f7e0c469eda121d5a7843d71c7c63537
Toaster <135201735+ToasterSSD@users.noreply.github.com>
2025-01-26 23:09:04 +0800

    dont need hcange

--- Android-Movie-Rater-Assignment-1-main/Android-Movie-Rater-Assignment-1-main/app/build.gradle.kts
+++ Android-Movie-Rater-Assignment-1-main/Android-Movie-Rater-Assignment-1-main/app/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
-    id("org.jetbrains.kotlin.plugin.serialization") version "1.8.10"
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.9.10"
     id("org.jetbrains.kotlin.plugin.compose")
     id("com.google.devtools.ksp") version "2.1.0-1.0.29"
 }
@@ -60,6 +60,9 @@ dependencies {
     implementation(libs.kotlinx.serialization.json)
     implementation(libs.retrofit2.kotlinx.serialization.converter)
     implementation(libs.okhttp)
+    implementation(libs.coil.compose)
+    implementation(libs.kotlinx.serialization.json.v160)
+    implementation(libs.androidx.compose.material3.material3)
 
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
@@ -68,7 +71,10 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.androidx.activity.compose.v180)
+    implementation(libs.androidx.material.icons.extended)
     implementation(libs.androidx.navigation.compose)
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
     implementation(libs.com.google.code.gson)
     implementation(libs.androidx.lifecycle.process)
     testImplementation(libs.junit)
@@ -84,5 +90,11 @@ dependencies {
     implementation(libs.androidx.room.ktx)
     implementation(libs.androidx.room.runtime)
     ksp("androidx.room:room-compiler:${rootProject.extra["room_version"]}")
+
+    androidTestImplementation(libs.androidx.junit.v115)
+
+    testImplementation(libs.kotlinx.coroutines.test)
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:${rootProject.extra["lifecycle_version"]}")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:${rootProject.extra["lifecycle_version"]}")
 
 }
--- Android-Movie-Rater-Assignment-1-main/Android-Movie-Rater-Assignment-1-main/build.gradle.kts
+++ Android-Movie-Rater-Assignment-1-main/Android-Movie-Rater-Assignment-1-main/build.gradle.kts
@@ -1,3 +1,5 @@
+@file:Suppress("DEPRECATION")
+
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 
 
@@ -6,6 +8,7 @@ buildscript {
     extra.apply {
         set("nav_version", "2.8.4")
         set("room_version", "2.6.1")
+        set("lifecycle_version", "2.8.7")
     }
 }
 
@@ -13,7 +16,11 @@ buildscript {
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
-    id("com.android.library") version "8.7.3" apply false
+    id("com.android.library") version "8.8.0" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.1.0" apply false
 
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
 }
