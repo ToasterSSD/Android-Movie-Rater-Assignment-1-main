e92408d4504ed34eb63e666ee36a381f9ef3349f
Toaster <135201735+ToasterSSD@users.noreply.github.com>
2025-06-06 11:27:13 +0800

    awd

--- Android-Movie-Rater-Assignment-1-main/Android-Movie-Rater-Assignment-1-main/build.gradle.kts
+++ Android-Movie-Rater-Assignment-1-main/Android-Movie-Rater-Assignment-1-main/build.gradle.kts
@@ -17,7 +17,7 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
 
-    id("com.android.library") version "8.10.0" apply false
+    id("com.android.library") version "8.10.1" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.1.0" apply false
 
 }
